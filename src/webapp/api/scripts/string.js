de2c1101b3e07ffb7cba5d8b1b69af94e4c9526d
Ryan Lee <ryanlee@zepheira.com>
2013-03-08 00:47:36 -0800

    eliminate terrible String modification in favor of string utilities

--- src/webapp/api/scripts/string.js
+++ src/webapp/api/scripts/string.js
@@ -3,47 +3,48 @@
  *==================================================
  */
 
-// This file is terrible and should really not be perpetuated.  Modifying
-// base language data types is bad engineering.  To be removed.
 define(function() {
-String.prototype.trim = function() {
-    return this.replace(/^\s+|\s+$/g, '');
-};
+    var StringUtils = {};
 
-String.prototype.startsWith = function(prefix) {
-    return this.length >= prefix.length && this.substr(0, prefix.length) == prefix;
-};
+    StringUtils.trim = function(s) {
+        return s.replace(/^\s+|\s+$/g, '');
+    };
 
-String.prototype.endsWith = function(suffix) {
-    return this.length >= suffix.length && this.substr(this.length - suffix.length) == suffix;
-};
+    StringUtils.startsWith = function(s, prefix) {
+        return s.length >= prefix.length && s.substr(0, prefix.length) === prefix;
+    };
 
-String.substitute = function(s, objects) {
-    var result = "";
-    var start = 0;
-    while (start < s.length - 1) {
-        var percent = s.indexOf("%", start);
-        if (percent < 0 || percent == s.length - 1) {
-            break;
-        } else if (percent > start && s.charAt(percent - 1) == "\\") {
-            result += s.substring(start, percent - 1) + "%";
-            start = percent + 1;
-        } else {
-            var n = parseInt(s.charAt(percent + 1));
-            if (isNaN(n) || n >= objects.length) {
-                result += s.substring(start, percent + 2);
+    StringUtils.endsWith = function(s, suffix) {
+        return s.length >= suffix.length && s.substr(s.length - suffix.length) === suffix;
+    };
+
+    StringUtils.substitute = function(s, objects) {
+        var result, start, percent, n;
+        result = "";
+        start = 0;
+        while (start < s.length - 1) {
+            percent = s.indexOf("%", start);
+            if (percent < 0 || percent === s.length - 1) {
+                break;
+            } else if (percent > start && s.charAt(percent - 1) === "\\") {
+                result += s.substring(start, percent - 1) + "%";
+                start = percent + 1;
             } else {
-                result += s.substring(start, percent) + objects[n].toString();
+                n = parseInt(s.charAt(percent + 1));
+                if (isNaN(n) || n >= objects.length) {
+                    result += s.substring(start, percent + 2);
+                } else {
+                    result += s.substring(start, percent) + objects[n].toString();
+                }
+                start = percent + 2;
             }
-            start = percent + 2;
         }
-    }
-    
-    if (start < s.length) {
-        result += s.substring(start);
-    }
-    return result;
-};
+        
+        if (start < s.length) {
+            result += s.substring(start);
+        }
+        return result;
+    };
 
-    return null;
+    return StringUtils;
 });

5084f96605e1bbe5646c21de931be54a42f4b854
Ryan Lee <ryanlee@zepheira.com>
2013-03-08 00:55:12 -0800

    baseUrl is temporary, based on using local development infrastructure; for publishing, it should be changed to the canonical SimileAjax URL for this version of itself

--- src/webapp/api/main.js
+++ src/webapp/api/main.js
@@ -1,28 +1,11 @@
-// Example development configuration for loading SimileAjax
 requirejs.config({
-    "baseUrl": "http://localhost/~ryanlee/dev/simile-ajax/scripts/",
-    "urlArgs": "bust=" + (new Date()).getTime(),
-    "paths": {
-        "lib": "../lib"
-    },
+    "baseUrl": "/ajax/api/",
     "shim": {
-        "lib/jquery": {
+        "./lib/jquery": {
             "exports": "jQuery"
         },
-        "lib/json2": {
+        "./lib/json2": {
             "exports": "JSON"
         }
     }
 });
-
-requirejs(
-    ["require", "../simile-ajax-api"],
-    function(require, SimileAjax) {
-        if (typeof window.JSON === "undefined" || window.JSON === null) {
-            window.JSON = require("lib/json2");
-        }
-        SimileAjax.load();
-        // part of the point of SimileAjax is being available everywhere
-        window.SimileAjax = SimileAjax;
-    }
-);

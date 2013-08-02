4054e8737362e7de3f41860b5037d0eaf50deae7
Ryan Lee <ryanlee@zepheira.com>
2013-08-02 13:29:13 -0700

    moving main to simile-ajax to reduce conflicts when using as a separate require path; eliminating simile-ajax-api since it just forwards to a bundle, building it as a bundle; renaming require bundle output to simile-ajax-bundle.js

--- optimize/build.js
+++ optimize/build.js
@@ -1,15 +1,16 @@
 ({
     "baseUrl": "../src/webapp/api/",
     "name": "lib/almond",
-    "include": [ "main" ],
+    "include": [ "simile-ajax" ],
     "wrap": {
         "startFile": "start.frag",
         "endFile": "end.frag"
     },
     "shim": {
-        "./lib/json2": {
+        "lib/json2": {
             "exports": "JSON"
         }
     },
-    "out": "../build/simile-ajax-bundle.js"
+    "deps": ["lib/json2"],
+    "out": "../build/simile-ajax-api.js"
 })

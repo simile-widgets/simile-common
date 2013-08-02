fba85a589c0acefbbe5c26d497fd993d04f4cb7c
Ryan Lee <ryanlee@zepheira.com>
2013-08-02 13:17:28 -0700

    shift function back to main file, take in bundle parameter solely for css (barely matters for this project

--- src/webapp/api/main.js
+++ src/webapp/api/main.js
@@ -74,6 +74,59 @@ define([
     };
 
     /**
+     * Parse out the query parameters from a URL
+     * @param {String} url    the url to parse, or location.href if undefined
+     * @param {Object} to     optional object to extend with the parameters
+     * @param {Object} types  optional object mapping keys to value types
+     *        (String, Number, Boolean or Array, String by default)
+     * @return a key/value Object whose keys are the query parameter names
+     * @type Object
+     */
+    SimileAjax.parseURLParameters = function(url, to, types) {
+        to = to || {};
+        types = types || {};
+        
+        if (typeof url == "undefined") {
+            url = location.href;
+        }
+        var q = url.indexOf("?");
+        if (q < 0) {
+            return to;
+        }
+        url = (url+"#").slice(q+1, url.indexOf("#")); // toss the URL fragment
+        
+        var params = url.split("&"), param, parsed = {};
+        var decode = window.decodeURIComponent || unescape;
+        for (var i = 0; param = params[i]; i++) {
+            var eq = param.indexOf("=");
+            var name = decode(param.slice(0,eq));
+            var old = parsed[name];
+            var replacement = decode(param.slice(eq+1));
+            
+            if (typeof old == "undefined") {
+                old = [];
+            } else if (!(old instanceof Array)) {
+                old = [old];
+            }
+            parsed[name] = old.concat(replacement);
+        }
+        for (var i in parsed) {
+            if (!parsed.hasOwnProperty(i)) continue;
+            var type = types[i] || String;
+            var data = parsed[i];
+            if (!(data instanceof Array)) {
+                data = [data];
+            }
+            if (type === Boolean && data[0] == "false") {
+                to[i] = false; // because Boolean("false") === true
+            } else {
+                to[i] = type.apply(this, data);
+            }
+        }
+        return to;
+    };
+
+    /**
      * @deprecated Use RequireJS loading mechanisms instead.
      */
     SimileAjax.includeJavascriptFile = function(doc, url, onerror, charset, callback) {
@@ -126,21 +179,39 @@ define([
 
     SimileAjax.load = function() {
         var cssFiles = [
-            "simile-ajax-bundle.css"
+            "main.css"
         ];
+        var bundledCssFile = "simile-ajax-bundle.css";
+        var params = {
+            "bundle": 
+        };
 
         if (typeof SimileAjax_urlPrefix == "string") {
             SimileAjax.urlPrefix = SimileAjax_urlPrefix;
         } else {
-            var url = SimileAjax.findScript(document, "simile-ajax-api.js");
+            var targets = ["simile-ajax-api.js", "simile-ajax-require-bundle.js"];
+            for (var i = 0; i < targets.length; i++) {
+                var target = targets[i];
+                var url = SimileAjax.findScript(document, target);
+                if (url != null) {
+                    SimileAjax.urlPrefix = url.substr(0, url.indexOf(target));
+                    break;
+                }
+            }
+
             if (url == null) {
                 SimileAjax.error = new Error("Failed to derive URL prefix for Simile Ajax API code files");
                 return;
             }
-            SimileAjax.urlPrefix = url.substr(0, url.indexOf("simile-ajax-api.js"));
+
+            params = SimileAjax.parseURLParameters(url, SimileAjax.params, SimileAjax.paramTypes);
         }
 
-        SimileAjax.includeCssFiles(document, SimileAjax.urlPrefix + "styles/", cssFiles);
+        if (params.bundle) {
+            SimileAjax.includeCssFile(document, SimileAjax.urlPrefix + "styles/" + bundleCssFile);
+        } else {
+            SimileAjax.includeCssFiles(document, SimileAjax.urlPrefix + "styles/", cssFiles);
+        }
         
         SimileAjax.loaded = true;
 
--- src/webapp/api/scripts/base.js
+++ src/webapp/api/scripts/base.js
@@ -3,64 +3,11 @@ define(function() {
         loaded:                 false,
         loadingScriptsCount:    0,
         error:                  null,
-        params:                 { bundle: "true" },
+        params:                 { "bundle": true },
+        paramTypes:             { "bundle": Boolean },
         version:                "3.0.0",
         jQuery:                 null, // use jQuery directly
         urlPrefix:              null
-    };
-
-
-    /**
-     * Parse out the query parameters from a URL
-     * @param {String} url    the url to parse, or location.href if undefined
-     * @param {Object} to     optional object to extend with the parameters
-     * @param {Object} types  optional object mapping keys to value types
-     *        (String, Number, Boolean or Array, String by default)
-     * @return a key/value Object whose keys are the query parameter names
-     * @type Object
-     */
-    SimileAjax.parseURLParameters = function(url, to, types) {
-        to = to || {};
-        types = types || {};
-        
-        if (typeof url == "undefined") {
-            url = location.href;
-        }
-        var q = url.indexOf("?");
-        if (q < 0) {
-            return to;
-        }
-        url = (url+"#").slice(q+1, url.indexOf("#")); // toss the URL fragment
-        
-        var params = url.split("&"), param, parsed = {};
-        var decode = window.decodeURIComponent || unescape;
-        for (var i = 0; param = params[i]; i++) {
-            var eq = param.indexOf("=");
-            var name = decode(param.slice(0,eq));
-            var old = parsed[name];
-            var replacement = decode(param.slice(eq+1));
-            
-            if (typeof old == "undefined") {
-                old = [];
-            } else if (!(old instanceof Array)) {
-                old = [old];
-            }
-            parsed[name] = old.concat(replacement);
-        }
-        for (var i in parsed) {
-            if (!parsed.hasOwnProperty(i)) continue;
-            var type = types[i] || String;
-            var data = parsed[i];
-            if (!(data instanceof Array)) {
-                data = [data];
-            }
-            if (type === Boolean && data[0] == "false") {
-                to[i] = false; // because Boolean("false") === true
-            } else {
-                to[i] = type.apply(this, data);
-            }
-        }
-        return to;
     };
     
     return SimileAjax;
